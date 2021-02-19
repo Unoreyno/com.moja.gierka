4e69f6d92cd24a00bd5267e6fb3a0ebfd5e0e5ae
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-19 19:43:46 +0100

    gierka 1

--- src/Frame.java
+++ src/Frame.java
@@ -1,14 +1,22 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Frame extends JFrame {
     private static final long serialVersionUID = 1L;
 
     public Frame(){
-        
+
         super("Kółko i krzyżyk");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 400);
         setLocation(300,100);
+
+        JPanel buttonsPanel = new Buttons();
+        add(buttonsPanel);
+        
+        
+
         setVisible(true);
 
 
