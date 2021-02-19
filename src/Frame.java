ba6ca3137740e57c8e7400de5e09263e898ce71b
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-19 18:11:45 +0100

    first commit

---
+++ src/Frame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public Frame(){
+        
+        super("Kółko i krzyżyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLocation(300,100);
+        setVisible(true);
+
+
+    }
+    
+
+}
