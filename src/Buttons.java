35cfede8aca53559e0004e5a73ac456c72c8e512
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-19 20:02:55 +0100

    gierka 2

--- src/Buttons.java
+++ src/Buttons.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 public class Buttons extends JPanel implements ActionListener{
     
     private static final long serialVersionUID = 1L;
+
+    boolean myTurn = true;
     JButton reset;
     JButton[] button = new JButton[9];
 
@@ -22,6 +24,7 @@ public class Buttons extends JPanel implements ActionListener{
 
         
         reset = new JButton("RESET");
+        reset.addActionListener(this);
         add(reset);
 
         setLayout(new GridLayout(4,3));
@@ -31,16 +34,28 @@ public class Buttons extends JPanel implements ActionListener{
 
     }
 
+    @Override
     
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
+        
+        if(source.equals(reset)){
+            for(int i = 0; i < button.length; i++){
+                button[i].setText("");
+            }
+        }
 
-
+        myTurn = !myTurn;
+        char symbol = 'X';
+        if(myTurn){
+            symbol = 'O';
+        }
 
         for(int i = 0; i < button.length; i++) {
             if(source.equals(button[i])){
-                button[i].setText("x");
+                button[i].setText(""+symbol);
             }
+            
         
         }
 
