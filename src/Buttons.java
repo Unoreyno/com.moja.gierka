c9b8f55a0ecc8c47043cebeb799bb8c69694bf24
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-22 16:29:02 +0100

    wysyłanko

--- src/Buttons.java
+++ src/Buttons.java
@@ -19,6 +19,7 @@ public class Buttons extends JPanel implements ActionListener{
             button[i] = new JButton("");
             button[i].addActionListener(this);
             add(button[i]);
+            button[i].setBackground(Color.WHITE);
 
         }
 
@@ -42,6 +43,7 @@ public class Buttons extends JPanel implements ActionListener{
         if(source.equals(reset)){
             for(int i = 0; i < button.length; i++){
                 button[i].setText("");
+                button[i].setEnabled(true);
             }
         }
 
@@ -54,6 +56,8 @@ public class Buttons extends JPanel implements ActionListener{
         for(int i = 0; i < button.length; i++) {
             if(source.equals(button[i])){
                 button[i].setText(""+symbol);
+                button[i].setEnabled(false);
+                // checkResult(i);
             }
             
         
@@ -61,4 +65,22 @@ public class Buttons extends JPanel implements ActionListener{
 
 
     }
+    /* public void checkResult(){
+
+
+        String[] ch = new String[3];
+    int[] id = new int[3];
+    for(int i = 0; i < 3; i++){
+      ch[i] = button[i].getText();
+      id[i] = i;
+    }
+    if(ch[0].equals(ch[1]) && ch[1].equals(ch[2]) && ch[0] != ""){
+      button[id[0]].setBackground(Color.GREEN);
+      button[id[1]].setBackground(Color.GREEN);
+      button[id[2]].setBackground(Color.GREEN);
+    } 
+
+
+    // checkResult(i);
+} */
 }
